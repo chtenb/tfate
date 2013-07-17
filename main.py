2d902cf5322016e0e1525ce34846d82049b821dc
Chiel92 <ctje92@gmail.com>
2013-07-17 21:31:34 +0200

    foo

--- main.py
+++ main.py
@@ -3,38 +3,68 @@ import sys
 sys.path.insert(0, '/home/chiel/Projects/protexted')
 
 import text
-import selections
+from selection import Selection
+import selectors
+import operators
 import curses
-from curses.wrapper import wrapper
+from curses import wrapper
 from curses.textpad import Textbox
 
 
 def main(stdscr):
     t = text.Text(sys.argv[1])
     curses.curs_set(0)
-    cursor_pos = 0
+    selection = Selection()
+    selection.add((0, 1))
 
     while 1:
-        stdscr.addstr(0, 0, t.get_interval(0, cursor_pos))
-        stdscr.addstr(t.get_interval(cursor_pos, cursor_pos + 1), curses.A_REVERSE)
-        stdscr.addstr(t.get_interval(cursor_pos + 1, len(t)))
+        stdscr.move(0, 0)
+        for interval_content, in_selection in t.partition_content(selection.partition(t)):
+            if in_selection:
+                stdscr.addstr(interval_content, curses.A_REVERSE)
+            else:
+                stdscr.addstr(interval_content)
+
         stdscr.addstr("EOF", curses.A_BOLD)
+        stdscr.addstr(str(selection), curses.A_BOLD)  # DEBUG
         stdscr.clrtobot()
         stdscr.refresh()
 
         key = stdscr.getch()
         if key == ord('j'):
-            cursor_pos = selections.next_line(t, cursor_pos)
-        if key == ord('k'):
-            cursor_pos = selections.previous_line(t, cursor_pos)
-        if key == ord('h'):
-            cursor_pos = max(cursor_pos - 1, 0)
-        if key == ord('l'):
-            cursor_pos = min(cursor_pos + 1, len(t))
-        if key == ord('i'):
-            insert_text = get_input(stdscr)
-            t.set_interval(cursor_pos, cursor_pos, insert_text)
-        if key == ord(':'):
+            selection = selectors.move_to_next_line(t, selection)
+        elif key == ord('k'):
+            selection = selectors.move_to_previous_line(t, selection)
+        elif key == ord('l'):
+            selection = selectors.move_to_next_char(t, selection)
+        elif key == ord('h'):
+            selection = selectors.move_to_previous_char(t, selection)
+        elif key == ord('i'):
+            insert_text = ""
+            while 1:
+                key2 = stdscr.getch()
+                if key2 == 27:
+                    t.apply_operation(operation)
+                    selection = operation.new_selection
+                    break;
+                else:
+                    insert_text += chr(key2)
+                    operation = operators.insert_before(t, selection, insert_text)
+
+            #insert_text = get_input(stdscr)
+            #if key2 == ord('a'):
+                #operation = operators.insert_before(t, selection, insert_text)
+            #elif key2 == ord('s'):
+                #operation = operators.insert_around(t, selection, insert_text)
+            #elif key2 == ord('d'):
+                #operation = operators.insert_in_place(
+                    #t, selection, insert_text)
+            #elif key2 == ord('f'):
+                #operation = operators.insert_after(t, selection, insert_text)
+            #else:
+                #continue
+
+        elif key == ord(':'):
             command = get_input(stdscr)
             try:
                 exec(command)
@@ -44,14 +74,15 @@ def main(stdscr):
                 stdscr.refresh()
                 stdscr.getch()
 
+
 def get_input(stdscr):
-    y,x = stdscr.getmaxyx()
-    stdscr.addstr(y - 1, 0, ">")
+    y, x = stdscr.getmaxyx()
+    stdscr.addstr(y - 1, 0, ">>")
     stdscr.refresh()
-    s = curses.newwin(1,x - 1,y - 1,1)
+    s = curses.newwin(1, x - 1, y - 1, 1)
     s.refresh()
     text_box = Textbox(s)
     text_box.edit()
-    return text_box.gather()
+    return text_box.gather()[:-1]
 
 wrapper(main)
