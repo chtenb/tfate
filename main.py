3daabff4f494b9e5114012471f185d54dfe8565a
Chiel92 <ctje92@gmail.com>
2013-07-18 14:35:19 +0200

    showing large files is fast now

--- main.py
+++ main.py
@@ -15,18 +15,31 @@ def main(stdscr):
     t = text.Text(sys.argv[1])
     curses.curs_set(0)
     selection = Selection()
+    operation = None
     selection.add((0, 1))
 
     while 1:
         stdscr.move(0, 0)
-        for interval_content, in_selection in t.partition_content(selection.partition(t)):
-            if in_selection:
-                stdscr.addstr(interval_content, curses.A_REVERSE)
-            else:
-                stdscr.addstr(interval_content)
 
-        stdscr.addstr("EOF", curses.A_BOLD)
-        stdscr.addstr(str(selection), curses.A_BOLD)  # DEBUG
+        lower_bound = 0
+        y,x = stdscr.getmaxyx()
+        upper_bound = y * x
+        doesnt_fit = False
+        bounded_selection = selection.bound(lower_bound, upper_bound)
+        bounded_partition = selection.partition(t).bound(lower_bound, upper_bound)
+        for interval in bounded_partition:
+            try:
+                if interval in bounded_selection:
+                    stdscr.addstr(t.interval_content(*interval), curses.A_REVERSE)
+                else:
+                    stdscr.addstr(t.interval_content(*interval))
+            except Exception as e:
+                doesnt_fit = True
+                break
+
+        if not doesnt_fit:
+            stdscr.addstr("EOF", curses.A_BOLD)
+            stdscr.addstr(str(selection), curses.A_BOLD)  # DEBUG
         stdscr.clrtobot()
         stdscr.refresh()
 
@@ -44,8 +57,9 @@ def main(stdscr):
             while 1:
                 key2 = stdscr.getch()
                 if key2 == 27:
-                    t.apply_operation(operation)
-                    selection = operation.new_selection
+                    if operation != None:
+                        t.apply_operation(operation)
+                        selection = operation.new_selection
                     break;
                 else:
                     insert_text += chr(key2)
