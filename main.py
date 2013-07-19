87bf18e3e4bbe865c23669567f6c4d1edcd45803
Chiel92 <ctje92@gmail.com>
2013-07-19 14:31:01 +0200

    operation realtime displayed

--- main.py
+++ main.py
@@ -16,33 +16,10 @@ def main(stdscr):
     t = text.Text(sys.argv[1])
     curses.curs_set(0)
     selection = Selection()
-    operation = None
     selection.add((0, 1))
 
     while 1:
-        stdscr.move(0, 0)
-
-        lower_bound = 0
-        y,x = stdscr.getmaxyx()
-        upper_bound = y * x
-        doesnt_fit = False
-        bounded_selection = bound(selection, lower_bound, upper_bound)
-        bounded_partition = bound(partition(selection, t), lower_bound, upper_bound)
-        for interval in bounded_partition:
-            try:
-                if interval in bounded_selection:
-                    stdscr.addstr(t.interval_content(*interval), curses.A_REVERSE)
-                else:
-                    stdscr.addstr(t.interval_content(*interval))
-            except Exception as e:
-                doesnt_fit = True
-                break
-
-        if not doesnt_fit:
-            stdscr.addstr("EOF", curses.A_BOLD)
-            stdscr.addstr(str(selection), curses.A_BOLD)  # DEBUG
-        stdscr.clrtobot()
-        stdscr.refresh()
+        draw_text(stdscr, t, selection)
 
         key = stdscr.getch()
         if key == ord('j'):
@@ -55,6 +32,7 @@ def main(stdscr):
             selection = selectors.move_to_previous_char(selection, t)
         elif key == ord('i'):
             insert_text = ""
+            operation = None
             while 1:
                 key2 = stdscr.getch()
                 if key2 == 27:
@@ -64,21 +42,9 @@ def main(stdscr):
                     break;
                 else:
                     insert_text += chr(key2)
-                    operation = operators.insert_before(t, selection, insert_text)
-
-            #insert_text = get_input(stdscr)
-            #if key2 == ord('a'):
-                #operation = operators.insert_before(t, selection, insert_text)
-            #elif key2 == ord('s'):
-                #operation = operators.insert_around(t, selection, insert_text)
-            #elif key2 == ord('d'):
-                #operation = operators.insert_in_place(
-                    #t, selection, insert_text)
-            #elif key2 == ord('f'):
-                #operation = operators.insert_after(t, selection, insert_text)
-            #else:
-                #continue
-
+                    operation = operators.insert_before(
+                        t, selection, insert_text)
+                draw_text(stdscr, t, selection, operation)
         elif key == ord(':'):
             command = get_input(stdscr)
             try:
@@ -99,5 +65,41 @@ def get_input(stdscr):
     text_box = Textbox(s)
     text_box.edit()
     return text_box.gather()[:-1]
+
+
+def draw_text(stdscr, t, selection, operation=None):
+        stdscr.move(0, 0)
+
+        lower_bound = 0
+        y, x = stdscr.getmaxyx()
+        upper_bound = y * x
+        doesnt_fit = False
+        bounded_selection = bound(selection, lower_bound, upper_bound)
+        bounded_partition = bound(
+            partition(selection, t), lower_bound, upper_bound)
+        selection_index = 0
+        for interval in bounded_partition:
+            try:
+                if interval in bounded_selection:
+                    if operation == None:
+                        stdscr.addstr(
+                            t.interval_content(interval), curses.A_REVERSE)
+                    else:
+                        stdscr.addstr(operation.new_content[
+                                      selection_index], curses.A_REVERSE)
+                    selection_index += 1
+                else:
+                    stdscr.addstr(t.interval_content(interval))
+            except Exception as e:
+                doesnt_fit = True
+                break
+
+        try:
+            stdscr.addstr("EOF", curses.A_BOLD)
+            stdscr.addstr(str(selection), curses.A_BOLD)  # DEBUG
+        except:
+            pass
+        stdscr.clrtobot()
+        stdscr.refresh()
 
 wrapper(main)
