cee313c89bd767093c9df564a0cd198ce5a35576
Chiel92 <ctje92@gmail.com>
2013-07-22 17:56:08 +0200

    status and text in seperate windows

--- main.py
+++ main.py
@@ -8,98 +8,104 @@ from selectors import bound, partition
 import selectors
 import operators
 import curses
-from curses import wrapper
 from curses.textpad import Textbox
 
 
-def main(stdscr):
-    t = text.Text(sys.argv[1])
-    curses.curs_set(0)
-    selection = Selection()
-    selection.add((0, 1))
+class UserInterface:
+    def __init__(self):
+        self.text = text.Text(sys.argv[1])
+        self.selection = Selection()
+        self.selection.add((0, 1))
+        self.screen_start = 0
 
-    while 1:
-        draw_text(stdscr, t, selection)
+    def main(self, stdscr):
+        self.stdscr = stdscr
+        curses.curs_set(0)
+        y, x = self.stdscr.getmaxyx()
+        self.text_win = curses.newwin(y - 1, x, 0, 0)
+        self.status_win = curses.newwin(1, x, y - 1, 0)
+        while 1:
+            self.draw_text_win()
+            self.user_input()
 
-        key = stdscr.getch()
+    def user_input(self):
+        key = self.stdscr.getch()
         if key == ord('j'):
-            selection = selectors.move_to_next_line(selection, t)
+            self.selection = selectors.move_to_next_line(self.selection, self.text)
         elif key == ord('k'):
-            selection = selectors.move_to_previous_line(selection, t)
+            self.selection = selectors.move_to_previous_line(self.selection, self.text)
         elif key == ord('l'):
-            selection = selectors.move_to_next_char(selection, t)
+            self.selection = selectors.move_to_next_char(self.selection, self.text)
         elif key == ord('h'):
-            selection = selectors.move_to_previous_char(selection, t)
+            self.selection = selectors.move_to_previous_char(self.selection, self.text)
         elif key == ord('i'):
-            insert_text = ""
-            operation = None
-            while 1:
-                key2 = stdscr.getch()
-                if key2 == 27:
-                    if operation != None:
-                        t.apply_operation(operation)
-                        selection = operation.new_selection
-                    break;
-                else:
-                    insert_text += chr(key2)
-                    operation = operators.insert_before(
-                        t, selection, insert_text)
-                draw_text(stdscr, t, selection, operation)
+            self.insert_mode(operators.insert_before)
+        elif key == ord('c'):
+            self.insert_mode(operators.insert_in_place)
         elif key == ord(':'):
-            command = get_input(stdscr)
+            command = self.get_input(self.status_win)
             try:
                 exec(command)
             except Exception as e:
-                stdscr.addstr(str(e))
-                stdscr.addstr("\nPress any key...")
-                stdscr.refresh()
-                stdscr.getch()
+                self.draw_status(str(e))
+            self.status_win.getch()
 
+    def insert_mode(self, operator):
+        insert_text = ""
+        operation = None
+        while 1:
+            key2 = self.stdscr.getch()
+            if key2 == 27:
+                if operation != None:
+                    self.text.apply_operation(operation)
+                    self.selection = operation.new_selection
+                break;
+            else:
+                insert_text += chr(key2)
+                operation = operator(self.text, self.selection, insert_text)
+            self.draw_text_win(operation)
 
-def get_input(stdscr):
-    y, x = stdscr.getmaxyx()
-    stdscr.addstr(y - 1, 0, ">>")
-    stdscr.refresh()
-    s = curses.newwin(1, x - 1, y - 1, 1)
-    s.refresh()
-    text_box = Textbox(s)
-    text_box.edit()
-    return text_box.gather()[:-1]
+    def get_input(self, scr):
+        y, x = scr.getmaxyx()
+        scr.addstr(y - 1, 0, ">>")
+        scr.refresh()
+        text_box = Textbox(scr)
+        text_box.edit()
+        return text_box.gather()[:-1]
 
-
-def draw_text(stdscr, t, selection, operation=None):
-        stdscr.move(0, 0)
+    def draw_text_win(self, operation=None):
+        self.text_win.move(0, 0)
 
         lower_bound = 0
-        y, x = stdscr.getmaxyx()
+        y, x = self.text_win.getmaxyx()
         upper_bound = y * x
-        doesnt_fit = False
-        bounded_selection = bound(selection, lower_bound, upper_bound)
-        bounded_partition = bound(
-            partition(selection, t), lower_bound, upper_bound)
+        bounded_selection = bound(self.selection, lower_bound, upper_bound)
+        bounded_partition = bound(partition(self.selection, self.text), lower_bound, upper_bound)
         selection_index = 0
-        for interval in bounded_partition:
-            try:
+        try:
+            for interval in bounded_partition:
                 if interval in bounded_selection:
                     if operation == None:
-                        stdscr.addstr(
-                            t.interval_content(interval), curses.A_REVERSE)
+                        self.text_win.addstr(self.text.interval_content(interval), curses.A_REVERSE)
                     else:
-                        stdscr.addstr(operation.new_content[
-                                      selection_index], curses.A_REVERSE)
+                        self.text_win.addstr(operation.new_content[selection_index], curses.A_REVERSE)
                     selection_index += 1
                 else:
-                    stdscr.addstr(t.interval_content(interval))
-            except Exception as e:
-                doesnt_fit = True
-                break
-
-        try:
-            stdscr.addstr("EOF", curses.A_BOLD)
-            stdscr.addstr(str(selection), curses.A_BOLD)  # DEBUG
+                    self.text_win.addstr(self.text.interval_content(interval))
+            self.text_win.addstr("EOF", curses.A_BOLD)
+            self.text_win.addstr(str(self.selection), curses.A_BOLD)  # DEBUG
         except:
             pass
-        stdscr.clrtobot()
-        stdscr.refresh()
+        self.text_win.clrtobot()
+        self.text_win.refresh()
 
-wrapper(main)
+    def draw_status(self, string):
+        try:
+            self.status_win.addstr(0, 0, string)
+        except:
+            pass
+        self.status_win.clrtobot()
+        self.status_win.refresh()
+
+ui = UserInterface()
+curses.wrapper(ui.main)
