6ba8c1052d7b9f650d62d45c41dd6f0ab9b95401
Chiel92 <ctje92@gmail.com>
2013-09-24 15:34:12 +0200

    setup script working

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name="pte",
+      version="0.0.1",
+      description="Terminal user interface for protexted",
+      author="Chiel ten Brinke",
+      author_email="ctenbrinke@gmail.com",
+      url="http://www.github.com/Chiel92/protexted",
+      packages=['protexted',
+          'protexted.user',
+          'protexted.undo_system',
+          'protexted.labeling_system',
+          'protexted.filetype_system'
+          ],
+      scripts=["pte"]
+      )
