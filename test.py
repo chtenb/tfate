2f04345a88c057a8562cd3f1bbedda94efa26c8d
Chiel92 <ctje92@gmail.com>
2013-12-02 22:43:41 +0100

    saved flag added

--- test.py
+++ test.py
@@ -9,16 +9,9 @@ import logging
 
 def main(filename):
     s = Session(filename)
-    s.selection = Selection()
-    print(bool(s.selection))
-    s.selection.add((3,3))
-    print(bool(s.selection))
-    s.selection.add((3,3))
-    print(s.selection)
-    s.selection.add((1,2))
-    print(s.selection)
-    o = operators.insert_before(s, s.selection, "Hello world!")
-    s.apply(o)
-    #IPython.embed()
+    selection = Selection(s, [(3,5), (8,9)])
+    selection1 = Selection(s, [(3,5), (8,9)])
+    print(selection == selection1)
+    print(selection != selection1)
 
 main("foo.txt")
