953c17a2b29dee210df25898f17d1d0df3832608
Chiel92 <ctje92@gmail.com>
2013-12-05 17:11:09 +0100

    setup script updated

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from distutils.core import setup
 
-setup(name="fate",
+setup(name="FATE",
       version="0.0.1",
-      description="Terminal user interface for fate",
+      description="Terminal user interface for FATE",
       author="Chiel ten Brinke",
       author_email="ctenbrinke@gmail.com",
       url="http://www.github.com/Chiel92/fate",
       packages=['fatecore',
           'fatecore.undo_system',
           'fatecore.labeling_system',
-          'fatecore.clipboard_system',
           'fatecore.filetype_system'
           ],
       scripts=["fate"]
