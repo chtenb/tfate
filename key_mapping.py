19e1500ae99bcbe0e5ce279aac021d187b5c9dfa
Chiel92 <ctje92@gmail.com>
2013-12-06 19:34:42 +0100

    License added and many stylfe fixes

--- key_mapping.py
+++ key_mapping.py
@@ -1,5 +1,5 @@
-"""This module contains the key mappings as two dictionaries from chars to functions. The first one, `actions`, maps chars to actions. The second one, `ui_actions`, maps chars to function which take an UserInterface object."""
-from fatecore import selectors, actions, operators, modes
+"""This module contains the key mapping as two dictionaries from chars to functions. The first one, `actions`, maps chars to actions. The second one, `ui_actions`, maps chars to function which take an UserInterface object."""
+from fatecore import selectors, actions, operators
 from fatecore.session import Session
 import re
 
@@ -31,14 +31,14 @@ actions = {
 }
 
 
-def quit(ui):
+def quit_session(ui):
     if not ui.session.saved:
         ui.set_status("Unsaved changes! Really quit? (y/n)")
         while 1:
-            c = chr(ui.stdscr.getch())
-            if c == 'y':
+            char = chr(ui.stdscr.getch())
+            if char == 'y':
                 exit()
-            if c == 'n':
+            if char == 'n':
                 break
     else:
         exit()
@@ -56,26 +56,26 @@ def local_find_backwards(ui):
 
 def search(ui):
     s = ui.session
-    s.searchPattern = ui.prompt('/')
-    s.apply(selectors.global_pattern_selector(s.searchPattern))
+    s.search_pattern = ui.prompt('/')
+    s.apply(selectors.global_pattern_selector(s.search_pattern))
 
 
 def search_current_content(ui):
     s = ui.session
-    s.searchPattern = re.escape(s.content(s.selection)[-1])
-    s.apply(selectors.global_pattern_selector(s.searchPattern))
+    s.search_pattern = re.escape(s.content(s.selection)[-1])
+    s.apply(selectors.global_pattern_selector(s.search_pattern))
 
 
 def search_next(ui):
     s = ui.session
-    if s.searchPattern:
-        s.apply(selectors.global_pattern_selector(s.searchPattern))
+    if s.search_pattern:
+        s.apply(selectors.global_pattern_selector(s.search_pattern))
 
 
 def search_previous(ui):
     s = ui.session
-    if s.searchPattern:
-        s.apply(selectors.global_pattern_selector(s.searchPattern, reverse=True))
+    if s.search_pattern:
+        s.apply(selectors.global_pattern_selector(s.search_pattern, reverse=True))
 
 
 def open_line_after(ui):
@@ -88,7 +88,7 @@ def open_line_before(ui):
     ui.insert_mode(operators.change_after)
 
 ui_actions = {
-    'Q': quit,
+    'Q': quit_session,
     'f': local_find,
     'F': local_find_backwards,
     '/': search,
--- user_interface.py
+++ user_interface.py
@@ -1,71 +1,78 @@
-from fatecore import session, selectors, operators, actions, modes
-from fatecore.selection import Selection
+"""This module contains the UserInterface class."""
+from fatecore.session import Session
+from fatecore import selectors, operators, actions, modes
 import key_mapping
 import curses
 from curses.textpad import Textbox
 import sys
-import re
 import logging
 
 
 class UserInterface:
+    """This class provides a user interface for interacting with a session object."""
     def __init__(self):
         if len(sys.argv) > 1:
-            self.session = session.Session(sys.argv[1])
+            self.session = Session(sys.argv[1])
         else:
-            self.session = session.Session()
+            self.session = Session()
         self.session.read()
-        self.session.searchPattern = ""
+        self.session.search_pattern = ""
+        self.mode = modes.SELECT_MODE
 
     def main(self, stdscr):
+        """Actually starts the user interface."""
         # Initialize color pairs from the terminal color palette
         curses.use_default_colors()
         for i in range(0, curses.COLORS):
-            curses.init_pair(i, i, -1);
-        curses.init_pair(9, -1, 0);
+            curses.init_pair(i, i, -1)
+        curses.init_pair(9, -1, 0)
 
         # Create curses windows
         self.stdscr = stdscr
         curses.curs_set(0)
-        y, x = self.stdscr.getmaxyx()
-        self.text_win = curses.newwin(y - 1, x, 0, 0)
-        self.status_win = curses.newwin(1, x, y - 1, 0)
+        ymax, xmax = self.stdscr.getmaxyx()
+        self.text_win = curses.newwin(ymax - 1, xmax, 0, 0)
+        self.status_win = curses.newwin(1, xmax, ymax - 1, 0)
         self.stdscr.refresh()
 
         # Enter the main loop
         while 1:
-            self.mode = self.session.selection_mode
+            self.mode = self.session.SELECTION_MODE
             self.draw_text()
-            self.selection_mode()
+            self.normal_mode()
 
-    def selection_mode(self):
+    def normal_mode(self):
+        """We are in normal mode."""
         key = chr(self.stdscr.getch())
-        s = self.session
 
         if key in key_mapping.actions:
-            s.apply(key_mapping.actions[key])
+            self.session.apply(key_mapping.actions[key])
         elif key in key_mapping.ui_actions:
             key_mapping.ui_actions[key](self)
         elif key == ':':
-            scope = vars(s)
-            for name in vars(session.Session).keys():
-                scope.update({name: eval('s.' + name)})
-            scope.update({'self': s})
-            scope.update({'selectors': selectors})
-            scope.update({'operators': operators})
-            scope.update({'actions': actions})
-            command = self.prompt(':')
-            try:
-                result = eval(command, scope)
-                if result != None:
-                    self.set_status(str(result))
-                    self.stdscr.getch()
-            except Exception as e:
-                self.set_status(command + ' : ' + str(e))
+            self.command_mode()
+
+    def command_mode(self):
+        """We are in command mode."""
+        session = self.session
+        scope = vars(session)
+        scope.update({'self': session})
+        scope.update({'selectors': selectors})
+        scope.update({'operators': operators})
+        scope.update({'actions': actions})
+        command = self.prompt(':')
+        try:
+            result = eval(command, scope)
+            if result != None:
+                self.set_status(str(result))
                 self.stdscr.getch()
-            self.status_win.clear()
+        except Exception as e:
+            self.set_status(command + ' : ' + str(e))
+            self.stdscr.getch()
+        self.status_win.clear()
 
     def insert_mode(self, operator_constructor):
+        """We are in insert mode."""
         self.mode = 'OPERATION'
         insertions = ''
         deletions = 0
@@ -75,7 +82,7 @@ class UserInterface:
             key = self.stdscr.getch()
             if key == 27:
                 self.session.apply(pending_operator)
-                break;
+                break
             elif key == curses.KEY_BACKSPACE:
                 if insertions:
                     insertions = insertions[:-1]
@@ -88,48 +95,48 @@ class UserInterface:
                 insertions += chr(key)
 
     def draw_operation_interval(self, interval, content):
+        """Draw an interval which is operated upon."""
         content = content.replace('\n', '↵\n') or 'ε'
         self.text_win.addstr(content, curses.A_BOLD | curses.A_REVERSE)
 
-    def draw_selection_interval(self, interval):
-        beg, end = interval
-        if end - beg > 0:
-            self.draw_interval(interval, selected=True)
-        else:
-            self.text_win.addstr('ε', curses.A_REVERSE)
-
     def draw_interval(self, interval, selected=False):
+        """Draw a regular interval."""
         beg, end = interval
-        for position in range(beg, end):
-            # Print next character of the interval
-            attribute = curses.A_NORMAL
-            char = self.session.text[position]
+        if end - beg == 0:
+            self.text_win.addstr('ε', curses.A_REVERSE)
+        else:
+            for position in range(beg, end):
+                # Print next character of the interval
+                attribute = curses.A_NORMAL
+                char = self.session.text[position]
 
-            # Apply attribute when char is selected
-            if selected:
-                attribute |= curses.A_REVERSE
-                # Display newline character explicitly when selected
-                if char == '\n':
-                    char = '↵\n'
+                # Apply attribute when char is selected
+                if selected:
+                    attribute |= curses.A_REVERSE
+                    # Display newline character explicitly when selected
+                    if char == '\n':
+                        char = '↵\n'
 
-            # Apply attribute if char is labeled
-            if position in self.session.labeling:
-                for i, label in enumerate(['string', 'number', 'keyword', 'comment']):
-                    if self.session.labeling[position] == label:
-                        attribute |= curses.color_pair(i + 1)
+                # Apply attribute if char is labeled
+                if position in self.session.labeling:
+                    for i, label in enumerate(['string', 'number', 'keyword', 'comment']):
+                        if self.session.labeling[position] == label:
+                            attribute |= curses.color_pair(i + 1)
 
-            self.text_win.addstr(char, attribute)
+                self.text_win.addstr(char, attribute)
 
     def draw_text(self, pending_operator=None):
+        """Draw the visible text in the text window."""
         self.text_win.move(0, 0)
 
         # Find a suitable starting position
-        y, x = self.text_win.getmaxyx()
+        ymax, xmax = self.text_win.getmaxyx()
         selection = self.session.selection
-        position = move_n_wrapped_lines_up(self.session.text, x, max(0, selection[0][0]), int(y / 2))
+        position = move_n_wrapped_lines_up(self.session.text, xmax, max(0, selection[0][0]), int(ymax / 2))
 
         try:
             # Find index of first selected interval that has to be drawn
+            index = 0
             for index in range(len(selection)):
                 if selection[index][1] > position:
                     break
@@ -142,9 +149,10 @@ class UserInterface:
                     if interval[0] <= position:
                         # Print selected interval
                         if pending_operator:
-                            self.draw_operation_interval(interval, pending_operator(self.session, preview=True).new_content[index])
+                            self.draw_operation_interval(interval,
+                                                         pending_operator(self.session, preview=True).new_content[index])
                         else:
-                            self.draw_selection_interval(interval)
+                            self.draw_interval(interval, selected=True)
                         position = interval[1]
                         index += 1
                     else:
@@ -160,36 +168,40 @@ class UserInterface:
 
             self.text_win.addstr('EOF\n', curses.A_BOLD)
         except curses.error:
-            # End of screen reached
+            # End of window reached
             pass
 
         try:
-            self.set_status(self.session.filename +
-                           ("*" if not self.session.saved else "") + " | " + str(self.session.filetype)
-                            + " | " + self.mode + " | " + str(self.session.selection))
+            self.set_status(self.session.filename + ("*" if not self.session.saved else "")
+                            + " | " + str(self.session.filetype)
+                            + " | " + self.mode
+                            + " | " + str(self.session.selection))
         except curses.error:
-            # End of screen reached
+            # End of window reached
             pass
 
         self.text_win.clrtobot()
         self.text_win.refresh()
 
     def set_status(self, string):
+        """Set the content of the status window."""
         self.status_win.bkgd(' ', curses.color_pair(9))
         try:
             self.status_win.addstr(0, 0, string, curses.color_pair(9))
-        except:
+        except curses.error:
+            # End of window reached
             pass
         self.status_win.clrtobot()
         self.status_win.refresh()
 
     def prompt(self, prompt_string='>'):
+        """Prompt the user for an input string."""
         self.status_win.clear()
-        y, x = self.stdscr.getmaxyx()
+        ymax, xmax = self.stdscr.getmaxyx()
         self.status_win.addstr(0, 0, prompt_string)
         self.status_win.refresh()
-        l = len(prompt_string)
-        text_box_win = curses.newwin(1, x - l, y - 1, l)
+        prompt_len = len(prompt_string)
+        text_box_win = curses.newwin(1, xmax - prompt_len, ymax - 1, prompt_len)
         text_box_win.bkgd(' ', curses.color_pair(9))
         text_box = Textbox(text_box_win)
         text_box.edit()
