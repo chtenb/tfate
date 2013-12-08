c4e650055c1b4726f8179629e7ebf7eb8ee788ec
Chiel92 <ctje92@gmail.com>
2013-12-08 16:36:15 +0100

    setup script fixed

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
 
-setup(name="FATE",
-      version="0.0.1",
-      description="Terminal user interface for FATE",
-      author="Chiel ten Brinke",
-      author_email="ctenbrinke@gmail.com",
-      url="http://www.github.com/Chiel92/fate",
+setup(name='FATE',
+      version='0.0.1',
+      description='Terminal user interface for FATE',
+      author='Chiel ten Brinke',
+      author_email='ctenbrinke@gmail.com',
+      url='http://www.github.com/Chiel92/fate',
+      package_dir={'fate': 'lib_fate/fate'},
       packages=[
           'tui',
-          'tui.fate',
-          'tui.fate.undo_system',
-          'tui.fate.labeling_system',
-          'tui.fate.filetype_system'
-          ],
-      scripts=["fate"]
+          'fate',
+          'fate.undo_system',
+          'fate.labeling_system',
+          'fate.filetype_system'
+      ],
+      scripts=['fate']
       )
