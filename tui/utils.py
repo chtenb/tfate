7610fa3d2593d9b81a01a7a50cf482012a4b0a13
Chiel92 <ctje92@gmail.com>
2014-06-01 20:29:26 +0200

    get_wch substituted fro getch again

--- tui/utils.py
+++ tui/utils.py
@@ -6,41 +6,38 @@ def getchar(stdscr):
     """Retrieve input character from user as a readable string."""
     while 1:
         try:
-            char = stdscr.get_wch()
+            char = curses.getch()
             break
         except:
             pass
 
-    if isinstance(char, str):
-        _ord = ord(char)
-
-        # Replace special characters with a readable string
-        if _ord == 27:
-            result = 'Esc'
-        elif _ord == 10:
-            result = '\n'
-        elif _ord == 9:
-            result = '\t'
-        elif _ord < 32:
-            result = curses.unctrl(char)
-            result = result.decode()
-            result = 'Ctrl-' + result[1]
-        else:
-            result = char
-
-    elif isinstance(char, int):
+    # Replace special characters with a readable string
+    if char == 27:
+        result = 'Esc'
+    elif char == 10:
+        result = '\n'
+    elif char == 9:
+        result = '\t'
+    elif char == curses.KEY_BACKSPACE:
+        result = '\b'
+    elif char < 32:
+        result = curses.unctrl(char)
+        result = result.decode()
+        result = 'Ctrl-' + result[1]
+    elif char < 256:
+        result = chr(char)
+    else:
         # char must be some kind of function key
-        if char == curses.KEY_BACKSPACE:
-            result = '\b'
-        else:
+        try:
             result = curses.keyname(char)
+        except:
+            raise IOError('Can\'t handle input character type: {}.'
+                            .format(str(type(char))))
+        else:
             result = result.decode()
             result = result[4] + result[5:].lower()
             # Remove parenthesis for function keys
             result.replace('(', '')
             result.replace(')', '')
-    else:
-        raise IOError('Can\'t handle input character type: {}.'
-                        .format(str(type(char))))
     debug(result)
     return result
