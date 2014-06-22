f0a542190133abdd364c5081a4b22073ccaee963
Chiel92 <ctje92@gmail.com>
2014-06-22 14:53:32 +0200

    fate moved to libs

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
-setup(name='fate',
+setup(name='tfate',
       version='0.0.1',
       description='Terminal user interface for fate',
       author='Chiel ten Brinke',
       author_email='ctenbrinke@gmail.com',
-      url='http://www.github.com/Chiel92/fate',
-      package_dir={'fate': 'lib_fate/fate'},
+      url='http://www.github.com/Chiel92/tfate',
+      package_dir={'fate': 'libs/fate'},
       packages=[
-          'tui',
           'fate',
+          'tui',
           'fate.labeling_system',
           'fate.filetype_system'
-      ],
+          ],
       scripts=['fate']
       )
