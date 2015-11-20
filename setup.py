becee03822b16b9b1ac7a06845731fbd36a1add8
Chiel ten Brinke <chiel.tenbrinke@intellimagic.com>
2015-11-20 22:48:35 +0100

    Make setup.py just copy filetree.

--- setup.py
+++ setup.py
@@ -2,26 +2,32 @@ from distutils.core import setup
 
 # Glob can't go recursively, so we do it manually
 import os
-ycm_data_files = []
-for root, dirnames, filenames in os.walk('libs/fate/fate/ycm/ycmd/'):
+all_files = []
+for root, dirnames, filenames in os.walk('.'):
     # Exclude all git related files
-    if root.find('.git') == -1:
-        filenames = [f for f in filenames if f.find('.git') == -1]
-        filenames = [f for f in filenames if f.find('.sh') == -1]
-        filenames = [f for f in filenames if f.find('.txt') == -1]
-        filenames = [f for f in filenames if f.find('.md') == -1]
-        #filenames = [f for f in filenames if f not in ['COPYING.txt', 'style_format.sh']]
+    if (root.find('.git') == -1
+            and root.find('build') == -1
+            and root.find('debug') == -1):
+        filenames = [root + '/' + f for f in filenames if f.find('.git') == -1
+                     and f.find('.sh') == -1
+                     and f.find('.txt') == -1
+                     and f.find('.md') == -1
+                     and f.find('.pyc') == -1
+                     and f.find('.swp') == -1
+                     and f.find('.yml') == -1
+                     and f != 'setup.py']
         if filenames:
-            #print(filenames)
-            ycm_data_files.append((root, filenames))
+            all_files.append(('tfate/' + root, filenames))
 
-#for filename in ycm_data_files:
-    #try:
-        #os.remove('/usr/local/'+os.path.basename(filename))
-    #except Exception as e:
-        #print(e)
-#print(ycm_data_files)
-#exit()
+print(all_files)
+
+# for filename in all_files:
+    # try:
+        # os.remove('/usr/local/'+os.path.basename(filename))
+    # except Exception as e:
+        # print(e)
+        # # print(ycm_data_files)
+        # # exit()
 
 # This globally installs the packages fate, tui and unicurses and the start script fate
 # Maybe its better to just copy the entire sourcetree as data_files, except for the .git dirs
@@ -34,23 +40,23 @@ setup(
     author='Chiel ten Brinke',
     author_email='ctenbrinke@gmail.com',
     url='http://www.github.com/Chiel92/tfate',
-    packages=[
-        'fate',
-        'tui',
-        'unicurses',
-        'fate.filetype',
-        'fate.formatting',
-        'fate.highlighting',
-        'fate.errorchecking',
-        'fate.ycm',
-        'fate.view',
-        'fate.test',
-    ],
-    package_dir={
-        'fate': 'libs/fate/fate',
-        'unicurses': 'libs/unicurses/unicurses'
-    },
-    data_files=ycm_data_files,
+    # packages=[
+        # 'fate',
+        # 'tui',
+        # 'unicurses',
+        # 'fate.filetype',
+        # 'fate.formatting',
+        # 'fate.highlighting',
+        # 'fate.errorchecking',
+        # #'fate.ycm',
+        # 'fate.view',
+        # 'fate.test',
+    # ],
+    # package_dir={
+        # 'fate': 'libs/fate/fate',
+        # 'unicurses': 'libs/unicurses/unicurses'
+    # },
+    data_files=all_files,
     scripts=['fate']
 )
 
