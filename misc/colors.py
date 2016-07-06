110ac12f20d0d163684238c97b14773f5e92eecf
Chiel ten Brinke <chiel.tenbrinke@intellimagic.com>
2016-07-06 18:15:24 +0200

    Update colors.py to work on windows

--- misc/colors.py
+++ misc/colors.py
@@ -1,6 +1,15 @@
 """This module is for messing with the terminal colors."""
-import curses
+import os
+import sys
+unicurses_path = os.path.dirname(os.path.abspath(__file__)) + '/../libs/unicurses'
+sys.path.insert(0, unicurses_path)
 
+import unicurses as curses
+
+tui_path = os.path.dirname(os.path.abspath(__file__)) + '/../tui'
+sys.path.insert(0, tui_path)
+
+import terminal
 
 def main(stdscr):
     """
@@ -12,42 +21,33 @@ def main(stdscr):
     The color pair 0 (mod 256) is fixed on (-1, -1).
     The colors 0 till 15 are the terminal palette colors.
     """
+    COLORS = 16
+    COLOR_PAIRS = 16
 
     # Print color support
-    stdscr.addstr('can_change_color: {}\n'.format(curses.can_change_color()))
-    stdscr.addstr('has_colors: {}\n'.format(curses.has_colors()))
-    stdscr.addstr('COLORS: {}\n'.format(curses.COLORS))
-    stdscr.addstr('COLOR_PAIRS: {}\n'.format(curses.COLOR_PAIRS))
-
-    stdscr.getch()
+    curses.addstr('can_change_color: {}\n'.format(curses.can_change_color()))
+    curses.addstr('has_colors: {}\n'.format(curses.has_colors()))
+    curses.addstr('COLORS: {}\n'.format(COLORS))
+    curses.addstr('COLOR_PAIRS: {}\n'.format(COLOR_PAIRS))
 
     # Init color options
     curses.start_color()
     curses.use_default_colors()
 
-    # Show color contents
-    # stdscr.addstr('colors:')
-    # for i in range(curses.COLORS):
-    #stdscr.addstr('{}: {}, '.format(str(i), curses.color_content(i)))
-    # for i in range(-1, 256):
-    #stdscr.addstr(str(i), i)
-    # stdscr.getch()
-    # exit()
-
     # Init color pairs
-    for i in range(curses.COLORS):
-        curses.init_pair(i + 1, i, 0)
-        curses.init_pair(i + 1 + curses.COLORS, i, 3)
-        #stdscr.addstr(str(curses.color_pair(i)) + ', ')
+    for i in range(COLORS):
+        curses.init_pair(i, i, 0)
+        curses.init_pair(i + COLORS, i, 3)
+        #curses.addstr(str(curses.color_pair(i)) + ', ')
 
     # Demonstrate color pairs
-    stdscr.addstr('\ncolorpairs: ')
-    for i in range(8):#range(curses.COLORS):
-        attribute = curses.color_pair(i + 1)
-        stdscr.addstr('color_pair: {}, attribute: {}\n'.format(i + 1, attribute), attribute)
+    curses.addstr('\ncolorpairs: \n')
+    for i in range(COLORS):
+        attribute = curses.color_pair(i)
+        curses.addstr('color_pair: {}, attribute: {}\n'.format(i, attribute), attribute)
 
     # Demonstrate attributes
-    stdscr.addstr('\nattributes: ')
+    curses.addstr('\nattributes: \n')
     for name in ['A_NORMAL',
                  'A_STANDOUT',
                  'A_UNDERLINE',
@@ -66,14 +66,16 @@ def main(stdscr):
                  'A_LEFT',
                  'A_RIGHT',
                  ]:
-        attribute = curses.__dict__[name]
-        #stdscr.addstr('name: {}, attribute: {}\n'.format(name, attribute), attribute)
+        try:
+            attribute = curses.__dict__[name]
+            curses.addstr('name: {}, attribute: {}\n'.format(name, attribute), attribute)
+        except KeyError:
+            curses.addstr('{} is not supported\n'.format(name))
 
-    #stdscr.addstr('\nattributes by number: ')
-    #for i in range(256*2):
-        #stdscr.addstr(str(i), i)
 
-    stdscr.refresh()
-    stdscr.getch()
+    curses.refresh()
+    curses.getch()
+    curses.endwin()
 
-curses.wrapper(main)
+terminal.init()
+main(terminal.stdscr)
